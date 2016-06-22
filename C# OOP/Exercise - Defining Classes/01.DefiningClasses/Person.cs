029af022f2b08420ecb6dedeba5c94f31bfcd277
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-06-22 22:20:10 +0300

    Adding exercise on defining classes

---
+++ C# OOP/Exercise - Defining Classes/01.DefiningClasses/Person.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace _01.DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            this.name = "No name";
+            this.age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(int age,string name) : this(age)
+        {
+            this.Name = name;
+        }
+        
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ C# OOP/Exercise - Defining Classes/01.DefiningClasses/StartUp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace _01.DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //Type personType = typeof(Person);
+            //FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            //Console.WriteLine(fields.Length);
+
+
+            //Type personType = typeof(Person);
+            //ConstructorInfo emptyCtor = personType.GetConstructor(new Type[] { });
+            //ConstructorInfo ageCtor = personType.GetConstructor(new[] { typeof(int) });
+            //ConstructorInfo nameAgeCtor = personType.GetConstructor(new[] { typeof(string), typeof(int) });
+            //bool swapped = false;
+            //if (nameAgeCtor == null)
+            //{
+            //    nameAgeCtor = personType.GetConstructor(new[] { typeof(int), typeof(string) });
+            //    swapped = true;
+            //}
+
+            //string name = Console.ReadLine();
+            //int age = int.Parse(Console.ReadLine());
+
+            //Person basePerson = (Person)emptyCtor.Invoke(new object[] { });
+            //Person personWithAge = (Person)ageCtor.Invoke(new object[] { age });
+            //Person personWithAgeAndName = swapped ? (Person)nameAgeCtor.Invoke(new object[] { age, name }) : (Person)nameAgeCtor.Invoke(new object[] { name, age });
+
+            //Console.WriteLine("{0} {1}", basePerson.name, basePerson.age);
+            //Console.WriteLine("{0} {1}", personWithAge.name, personWithAge.age);
+            //Console.WriteLine("{0} {1}", personWithAgeAndName.name, personWithAgeAndName.age);
+
+
+            //var peshoPerson = new Person
+            //{
+            //    name = "Pesho",
+            //    age = 20
+            //};
+
+            //var goshoPerson = new Person
+            //{
+            //    name = "Gosho",
+            //    age = 18
+            //};
+
+            //var stamatPerson = new Person();
+            //stamatPerson.name = "Stamat";
+            //stamatPerson.age = 43;
+
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                var person = new Person(int.Parse(input[1]), input[0]);
+
+                persons.Add(person);
+            }
+
+            persons.Where(p => p.Age > 30).OrderBy(p => p.Name).ToList().ForEach(p => Console.WriteLine($"{p.Name} - {p.Age}"));
+        }
+    }
+}
