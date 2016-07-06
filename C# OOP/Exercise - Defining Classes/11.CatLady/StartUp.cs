88443a690c29a3835da89ff75340393d54f7c7a7
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-07-07 02:51:43 +0300

    update

---
+++ C# OOP/Exercise - Defining Classes/11.CatLady/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _11.CatLady
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string cat = Console.ReadLine().Trim();
+            List<string> cats = new List<string>();
+
+            while (cat != "End")
+            {
+                cats.Add(cat);
+
+                cat = Console.ReadLine().Trim();
+            }
+            string searchedCat = Console.ReadLine().Trim();
+
+            foreach (var el in cats)
+            {
+                if (el.Contains(" " + searchedCat))
+                {
+                    Console.WriteLine(el);
+                }
+            }
+        }
+    }
+}
