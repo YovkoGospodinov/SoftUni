ccabb1e86cd713a81da93e360b2ce31e353e9c1d
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-07-23 16:36:24 +0300

    updating solutions

---
+++ C# OOP Advanced/Exercise - Enums and Attributes/07.DeckOfCards/Card.cs
@@ -0,0 +1,19 @@
+﻿namespace _07.DeckOfCards
+{
+    public class Card
+    {
+        private Rank rank;
+        private Suit suit;
+
+        public Card(Rank rank, Suit suit)
+        {
+            this.rank = rank;
+            this.suit = suit;
+        }
+
+        public string CardInfo()
+        {
+            return $"{this.rank} of {this.suit}";
+        }
+    }
+}
---
+++ C# OOP Advanced/Exercise - Enums and Attributes/07.DeckOfCards/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.DeckOfCards
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Card> cards = new List<Card>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    var currentCard = new Card((Rank)j, (Suit)i);
+                    cards.Add(currentCard);
+                }
+            }
+
+            foreach (var card in cards)
+            {
+                Console.WriteLine(card.CardInfo());
+            }
+        }
+    }
+}
