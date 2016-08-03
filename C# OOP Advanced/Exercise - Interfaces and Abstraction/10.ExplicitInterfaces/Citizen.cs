7dcd9d86919a712829981eac5cd0f0ff6dc86970
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-08-04 02:36:58 +0300

    updating with new solution

---
+++ C# OOP Advanced/Exercise - Interfaces and Abstraction/10.ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,29 @@
+﻿namespace _10.ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        private string name;
+
+        public Citizen(string name)
+        {
+            this.name = name;
+        }
+
+        string IPerson.Name {get;}
+
+        string IResident.Name { get; }
+
+        public string Country { get; }
+        public int Age { get; }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.name}";
+        }
+        
+        string IPerson.GetName()
+        {
+            return $"{this.name}";
+        }
+    }
+}
---
+++ C# OOP Advanced/Exercise - Interfaces and Abstraction/10.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _10.ExplicitInterfaces
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] citizenDetails = Console.ReadLine().Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            while (citizenDetails[0] != "End")
+            {
+                string name = citizenDetails[0];
+
+                Citizen citizen = new Citizen(name);
+
+                IPerson pCitizen = (IPerson) citizen;
+                IResident rCitizen = (IResident) citizen;
+ 
+                Console.WriteLine(pCitizen.GetName());
+                Console.WriteLine(rCitizen.GetName());
+
+                citizenDetails = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
